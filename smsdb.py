56445c9c2746991174b3426a345c1cc3cb81b08c
李皓骅 <lihaohua@xiaomi.com>
2014-10-11 17:32:29 +0800

    add_new_sms
    Signed-off-by: 李皓骅 <lihaohua@xiaomi.com>

--- smsdb.py
+++ smsdb.py
@@ -7,7 +7,7 @@ sms db
 import sqlite3
 conn = sqlite3.connect('sms.db')
 c = conn.cursor()
-CREATE_SQL = '''CREATE TABLE [sms] (
+CREATE_SQL = '''CREATE TABLE IF NOT EXISTS [sms] (
   [id] INTEGER PRIMARY KEY, 
   [user] TEXT, 
   [ip] TEXT, 
@@ -21,8 +21,13 @@ CREATE_SQL = '''CREATE TABLE [sms] (
 '''
 c.execute(CREATE_SQL)
 def add_new_sms(to_address, message):
-    return True
+    c.execute("INSERT INTO sms (to_address, message) VALUES (?, ?)", (to_address, message))
+    result = c.lastrowid
+    conn.commit()
+    return result > 0
 def get_sms_task():
     return None
 def set_sms_sent_to_worker(sms_id):
     return True
+if __name__ == '__main__':
+    add_new_sms('+8618601065423', '\x00\x01\x02TEST\x60')
