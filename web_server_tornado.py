54eafb20ed106abbba0d0d17cb47342e9b692173
eggfly <lihaohua90@gmail.com>
2014-10-15 15:55:24 +0000

    server comet!
    Signed-off-by: eggfly <lihaohua90@gmail.com>

--- web_server_tornado.py
+++ web_server_tornado.py
@@ -2,6 +2,7 @@
 
 import tornado.ioloop
 import tornado.web
+import logging
 
 class SmsQueue:
     def __init__(self):
@@ -13,35 +14,47 @@ class SmsQueue:
         return result
     def enqueue(self, sms):
         self.queue.append(sms)
-
+    def dump(self):
+        logging.warn(self.queue)
 workers = []
 sms_queue = SmsQueue()
-
+def try_send_one_message():
+    if len(workers) > 0:
+        handler = workers[0]
+        msg = sms_queue.dequeue()
+        sms_queue.dump()
+        if msg is not None:
+            send(handler, msg)
 def send(handler, message):
     handler.write(message)
     handler.finish()
     workers.remove(handler)
+    print "workers after send:", workers
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("get /")
-        print "get /"
+        self.write("mainpage")
+class TestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("get /test")
+        print "get /test"
         sms_queue.enqueue('**message**')
-        if len(workers) > 0:
-            handler = workers[0]
-            msg = sms_queue.dequeue()
-            if msg is not None:
-                send(handler, msg)
+        sms_queue.dump()
+        try_send_one_message()
 class CometHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     def get(self):
         print "get /comet"
         workers.append(self)
+        print "workers after append:", workers
+        try_send_one_message()
     def on_connection_close(self):
         print "get /comet closed"
         workers.remove(self)
+    print "workers after send:", workers
 application = tornado.web.Application([
-    (r"/comet", CometHandler),
     (r"/", MainHandler),
+    (r"/comet", CometHandler),
+    (r"/test", TestHandler),
 ])
 
 if __name__ == "__main__":
